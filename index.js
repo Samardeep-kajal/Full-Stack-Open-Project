d22656f77185c9c35ae5ba2f359d6b671ae0f375
Samardeep kajal <samk712003@gmail.com>
2022-03-23 21:14:20 +0530

    Fixing some bugs

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.post("/api/persons", (request, response) => {
   persons = persons.concat(person);
   response.json(person);
 });
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
